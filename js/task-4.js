fb0cd86fa7516603113680a07c8a111ca841c1fe
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-03-14 12:34:39 +0200

    task-4-js

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,17 @@
+const form = document.querySelector(".login-form");
+form.addEventListener("submit", handleSubmit);
 
+function handleSubmit(event) {
+    event.preventDefault();
+    if (event.target.elements.email.value === "" || event.target.elements.password.value === "") {
+        alert('All form fields must be filled in');
+    } else {
+        const ourElements = event.target.elements;
+        const info = {
+        email: ourElements.email.value.trim(),
+        password: ourElements.password.value.trim()
+        }
+        console.log(info);
+    }
+    event.target.reset();
+}
