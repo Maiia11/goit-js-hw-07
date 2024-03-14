5802852da246612cb4784fcb51c55b0ef1af9407
Maiia11 <149806655+Maiia11@users.noreply.github.com>
2024-03-14 13:41:33 +0200

    task-5-js

--- js/task-5.js
+++ js/task-5.js
@@ -1,3 +1,16 @@
+const div = document.querySelector(".widget");
+const butn = document.querySelector(".change-color");
+const span = document.querySelector(".color")
+console.log(span);
+console.log(butn);
+butn.addEventListener("click", handleClick);
+function handleClick() {
+  span.textContent = getRandomHexColor();
+  div.style.backgroundColor = getRandomHexColor();
+  return
+ 
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
